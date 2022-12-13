b750c97635f54326ffa15699d5f7464c56587803
Viki <hi@viki.moe>
2022-12-13 09:17:05 +0800

    feat: 开坑 Pixiv

---
+++ README.md
@@ -0,0 +1,24 @@
+# Pixiv for KiviBot
+
+![npm (scoped)](https://img.shields.io/npm/v/kivibot-plugin-pixiv?color=527dec&label=kivibot-plugin-pixiv&style=flat-square)
+[![dm](https://shields.io/npm/dm/kivibot-plugin-pixiv?style=flat-square)](https://www.npmjs.com/package/kivibot-plugin-pixiv)
+
+`KiviBot` 的 [Pixiv](https://pixiv.net/) 涩图插件（WIP）。
+
+**安装**
+
+```shell
+/plugin add pixiv
+```
+
+**启用**
+
+```shell
+/plugin on pixiv
+```
+
+**使用**
+
+```shell
+
+```
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { KiviPlugin, segment, http } from '@kivibot/core'
+
+const { version } = require('../package.json')
+const plugin = new KiviPlugin('Pixiv', version)
+
+const config = {}
+
+plugin.onMounted(() => {
+  Object.assign(config, plugin.loadConfig())
+  plugin.saveConfig(config)
+
+  plugin.onMessage(event => {
+    const { raw_message } = event
+
+    if (raw_message === 'hello') {
+      const msgs = [segment.face(66), 'world']
+      event.reply(msgs)
+    }
+  })
+})
+
+export { plugin }
